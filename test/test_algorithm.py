ef449f7b8d35cdec06819c6923a993541ff8b8c8
gisfromscratch <gisfromscratch@live.de>
2021-05-16 16:12:05 +0200

    Renamed the count test method

--- test/test_algorithm.py
+++ test/test_algorithm.py
@@ -4,7 +4,7 @@ from algorithm import count_int
 
 class TestAlgorithmMethods(unittest.TestCase):
 
-    def test_help(self):
+    def test_count(self):
         integers = [1, 2, 3, 4, 5, 1, 7]
         self.assertEqual(2, count_int(integers, 1), 'The list contain two elements being 1!')
 
